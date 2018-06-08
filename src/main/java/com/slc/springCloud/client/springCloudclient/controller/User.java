0050d75869d88a5a5a1fc70e2514d7801cd52d96
shileichao <shileichao@auto-link.local>
2018-06-08 16:29:51 +0800

    delay return

--- src/main/java/com/slc/springCloud/client/springCloudclient/controller/User.java
+++ src/main/java/com/slc/springCloud/client/springCloudclient/controller/User.java
@@ -5,13 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 /**
  * Created by shileichao on 2018/5/30.
  */
 @RestController
 public class User {
     @GetMapping("/user/findById")
-    public String findById(@RequestParam("id") String id) {
+    public String findById(@RequestParam("id") String id)throws Exception {
+        int i = new Random().nextInt(3000);
+        System.out.println("client线程休眠时间："+i);
+        Thread.sleep(i);
         System.out.println("这个是springcloud的客户端1----" + id);
         return "这个是springcloud的客户端1----" + id;
     }
