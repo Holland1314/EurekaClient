99f1d44134aff8587f10048fd77bf77ade6e640f
shileichao <shileichao@auto-link.local>
2018-08-23 18:08:21 +0800

    modify pom to 2.0.4

--- src/main/java/com/slc/springCloud/client/springCloudclient/SpringCloudClientApplication.java
+++ src/main/java/com/slc/springCloud/client/springCloudclient/SpringCloudClientApplication.java
@@ -2,10 +2,12 @@ package com.slc.springCloud.client.springCloudclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
-@EnableEurekaClient
+@SpringCloudApplication
+/*@SpringBootApplication
+@EnableEurekaClient*/
 public class SpringCloudClientApplication {
 
 	public static void main(String[] args) {
