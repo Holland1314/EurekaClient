e95c08c663dd7120bc295b0e277558d67ce9f20e
shileichao <shileichao@auto-link.local>
2018-08-24 16:10:46 +0800

    modify log

--- src/main/java/com/slc/springCloud/client/springCloudclient/controller/User.java
+++ src/main/java/com/slc/springCloud/client/springCloudclient/controller/User.java
@@ -14,9 +14,9 @@ import java.util.Random;
 public class User {
     @GetMapping("/user/findById")
     public String findById(@RequestParam("id") String id)throws Exception {
-        int i = new Random().nextInt(3000);
+      /*  int i = new Random().nextInt(3000);
         System.out.println("client线程休眠时间："+i);
-        Thread.sleep(i);
+        Thread.sleep(i);*/
         System.out.println("这个是springcloud的客户端1----" + id);
         return "这个是springcloud的客户端1----" + id;
     }
